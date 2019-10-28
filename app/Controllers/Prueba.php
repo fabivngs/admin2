b8e7fdc3d29e5ebe542f1bf9ae460781d613a26c
fabian <fabivngs@gmail.com>
2019-10-28 17:05:38 +0000

    primer commit

---
+++ app/Controllers/Prueba.php
@@ -0,0 +1,20 @@
+<?php namespace App\Controllers;
+
+class Prueba extends BaseController
+{
+	public function index()
+	{
+		//return view('welcome_message');
+    echo "esto es la funcion index";
+	}
+
+  public function test(){
+    echo "esto es una test1";
+  }
+
+  public function test2(){
+    echo "esto es una test2";
+  }
+	//--------------------------------------------------------------------
+
+}
